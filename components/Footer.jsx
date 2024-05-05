fd7a751bb062ce7a795059ccba72b43030caf02e
Harsh Shukla <shuklaharsh83267@gmail.com>
2024-05-05 12:08:11 +0530

    Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -39,7 +39,7 @@ const Footer = () => (
             METAVERUS
           </h4>
           <p className="font-normal text-[14px] text-white opacity-50">
-            Copyright © 2021 - 2022 Metaversus. All rights reserved.
+            Copyright © 2024 - 2025 Metaversus. All rights reserved.
           </p>
 
           <div className="flex gap-4">
